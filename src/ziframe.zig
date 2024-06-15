d2c07305e0cf2ba150971308d0d104494b8443dc
Adwaith Rajesh <adwaithrajesh3180@gmail.com>
2024-06-16 00:20:12 +0530

    Added function to init, append row, and print a DataFrame

--- src/ziframe.zig
+++ src/ziframe.zig
@@ -1,6 +1,54 @@
 const std = @import("std");
+const meta = std.meta;
 const stdout = std.io.getStdOut().writer();
 
-pub fn t() !void {
-    try stdout.print("hello world\n", .{});
+const Allocator = std.mem.Allocator;
+const ArrayList = std.ArrayList;
+
+pub fn DataFrame(comptime Columns: type) type {
+    if (!(@typeInfo(Columns) == .Struct)) {
+        @compileError("Columns must be of type struct, found " ++ @typeName(Columns));
+    }
+
+    const column_fields = meta.fields(Columns);
+
+    // THE DATA FRAME
+    return struct {
+        data: _DataFrame,
+
+        const Self = @This();
+
+        const _DataFrame = ArrayList(Columns);
+
+        /// returns an empty DataFrame
+        pub fn init(alloc: Allocator) Self {
+            return .{
+                .data = _DataFrame.init(alloc),
+            };
+        }
+
+        /// Add a new row to the DataFrame
+        pub fn append(self: *Self, data: Columns) !void {
+            try self.data.append(data);
+        }
+
+        /// release all memory allocated by the DataFrame
+        pub fn deinit(self: Self) void {
+            self.data.deinit();
+        }
+
+        /// format the DataFrame nicely
+        pub fn format(self: Self, comptime fmt: []const u8, options: std.fmt.FormatOptions, writer: anytype) !void {
+            _ = options;
+            _ = fmt;
+            inline for (column_fields) |cf| {
+                try writer.print("{s} ", .{cf.name});
+            }
+            try writer.print("\n", .{});
+
+            for (self.data.items, 0..) |row, idx| {
+                try writer.print("{} {any}\n", .{ idx, row });
+            }
+        }
+    };
 }
--- test/sanity.zig
+++ test/sanity.zig
@@ -1,5 +1,26 @@
+const std = @import("std");
+
 const zf = @import("ziframe");
 
 pub fn main() !void {
-    try zf.t();
+    var gpa = std.heap.GeneralPurposeAllocator(.{}){};
+    defer _ = gpa.deinit();
+
+    const alloc = gpa.allocator();
+
+    const ColValue = struct {
+        id: u16,
+        age: u32,
+    };
+
+    var df = zf.DataFrame(ColValue).init(alloc);
+    defer df.deinit();
+
+    try df.append(.{ .id = 10, .age = 10 });
+    try df.append(.{ .id = 11, .age = 11 });
+    try df.append(.{ .id = 12, .age = 14 });
+    try df.append(.{ .id = 13, .age = 20 });
+    try df.append(.{ .id = 14, .age = 19 });
+
+    std.debug.print("{}\n", .{df});
 }
